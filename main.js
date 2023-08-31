8b0a91107f14e7b4919eba9437576a6ffc6c39e0
August <augustjp@live.dk>
2023-08-31 22:32:07 +0200

    yeeaa22

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 //https://mlbkadeqohvoalvuqhec.supabase.co
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sYmthZGVxb2h2b2FsdnVxaGVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM1MDgzNjYsImV4cCI6MjAwOTA4NDM2Nn0._KaG9H7dvxEtQBOqnpjnmdLml5vjSigGXSXpGtmHur8
-fetch("https://mlbkadeqohvoalvuqhec.supabase.co/rest/v1/test_items", {
+fetch("https://mlbkadeqohvoalvuqhec.supabase.co/rest/v1/brands", {
     method:"GET",
     headers: {
-        "apikey":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sYmthZGVxb2h2b2FsdnVxaGVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM1MDgzNjYsImV4cCI6MjAwOTA4NDM2Nn0._KaG9H7dvxEtQBOqnpjnmdLml5vjSigGXSXpGtmHur8"
-    }
+        apikey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sYmthZGVxb2h2b2FsdnVxaGVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM1MDgzNjYsImV4cCI6MjAwOTA4NDM2Nn0._KaG9H7dvxEtQBOqnpjnmdLml5vjSigGXSXpGtmHur8"
+    },
 })
     .then(res=>res.json())
     .then(showData)
 
 function showData(items){
-    console.log(items);
+    console.table(items);
     // items.forEach()
 }
 
