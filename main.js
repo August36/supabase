718bc0a85e83b3a6bf825642ef4c1641ddd78f49
August <augustjp@live.dk>
2023-09-04 12:57:54 +0200

    linked script now it works xD

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 //https://mlbkadeqohvoalvuqhec.supabase.co
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sYmthZGVxb2h2b2FsdnVxaGVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM1MDgzNjYsImV4cCI6MjAwOTA4NDM2Nn0._KaG9H7dvxEtQBOqnpjnmdLml5vjSigGXSXpGtmHur8
 fetch("https://mlbkadeqohvoalvuqhec.supabase.co/rest/v1/brands", {
-    method:"GET",
-    headers: {
-        apikey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sYmthZGVxb2h2b2FsdnVxaGVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM1MDgzNjYsImV4cCI6MjAwOTA4NDM2Nn0._KaG9H7dvxEtQBOqnpjnmdLml5vjSigGXSXpGtmHur8"
-    },
+  method: "GET",
+  headers: {
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sYmthZGVxb2h2b2FsdnVxaGVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM1MDgzNjYsImV4cCI6MjAwOTA4NDM2Nn0._KaG9H7dvxEtQBOqnpjnmdLml5vjSigGXSXpGtmHur8",
+  },
 })
-    .then(res=>res.json())
-    .then(showData)
+  .then((res) => res.json())
+  .then(showData);
 
-function showData(items){
-    console.table(items);
-    // items.forEach()
+function showData(items) {
+  console.table(items);
+  // items.forEach()
 }
 
 // function (){}
